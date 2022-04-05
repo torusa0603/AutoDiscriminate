025403f55a3969eeb7fc3faab73686ddf35ae2a8
悟 比良 <S150231@sintokogio.jp>
2022-04-05 11:59:28 +0900

    gitignore追加 白ビットを無視して色判別を行う

--- AutoDiscriminate.py
+++ AutoDiscriminate.py
@@ -3,5 +3,5 @@ import sys
 import re
 
 result_folder_path = str(sys.argv[1])
-mWatershed.main(False, result_folder_path, False)
+mWatershed.main(False, result_folder_path, False, True)
 print("end")
--- mWatershed.py
+++ mWatershed.py
@@ -6,10 +6,24 @@ import cHandleJsonfile
 import os
 import math
 
-def main(nbShowFlag, nstrResultFolderPath, nbDetail):
+m_dicSettingElements={"NumberPerOneMiliMeter" : "", "MaxRadius" : "","circle_level_threshold":"","white_satuation":""} #設定ファイルから読み込む変数群を格納した辞書型変数 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### メイン関数
+def main(nbShowFlag, nstrResultFolderPath, nbDetail, nbLimitFileName):
   if nstrResultFolderPath == "":
     nstrResultFolderPath = "."
-  for fn in glob.glob(os.path.join(nstrResultFolderPath, "img/*")):
+  #設定ファイルを読み込む
+  cHandleJsonfile.ReadElementsFromJsonfile(os.path.join(nstrResultFolderPath,"Setting.json"), 'mWaterShed', m_dicSettingElements)
+  str_file_name = ""
+  if nbLimitFileName:
+    #img.pngというファイル名のみ解析する
+    #アプリに組み込む際は限定しておいた方が余計なファイルに惑わされない
+    str_file_name = "img/img.png"
+  else:
+    #PNGファイルなら何でも解析する
+    str_file_name = "img/*.png"
+  for fn in glob.glob(os.path.join(nstrResultFolderPath, str_file_name)):
     #画像の読み込み
     img_raw = cv2.imread(fn)
     if nbShowFlag:
@@ -59,17 +73,24 @@ def main(nbShowFlag, nstrResultFolderPath, nbDetail):
     #WaterShedを行う
     b_ret, i_number_labels, arr_center,  i_number_of_color_and_radius, npdata_number_of_color_and_radius = doWatershedMethod(img_raw, img_bind_mask, nstrResultFolderPath, nbShowFlag, nbDetail, i_count)
     if b_ret:
-      np.savetxt(os.path.join(nstrResultFolderPath, "result/color_radius.csv"), i_number_of_color_and_radius, delimiter=",", fmt="%d")
       if nbDetail:
+        color_band = ["R","Y","G","B","V","W"]
         for i in range(i_number_labels):
           cv2.putText(img_raw, "ID: " +str(i + 1),                                      ((int)(arr_center[i][0] - 40),(int)(arr_center[i][1] + 30)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0))
-          cv2.putText(img_raw, "Color: " +str(npdata_number_of_color_and_radius[i][0]), ((int)(arr_center[i][0] - 40),(int)(arr_center[i][1] + 60)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0))
+          cv2.putText(img_raw, "Color: " + color_band[npdata_number_of_color_and_radius[i][0]], ((int)(arr_center[i][0] - 40),(int)(arr_center[i][1] + 60)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0))
           cv2.putText(img_raw, "Size: " +str(npdata_number_of_color_and_radius[i][1]),  ((int)(arr_center[i][0] - 40),(int)(arr_center[i][1] + 90)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0))
           cv2.imwrite(os.path.join(nstrResultFolderPath, "img/result.png"), img_raw)
       if nbShowFlag:
         showPicture(img_raw, 'img_marked')
+    else:
+      #0埋めのリストを作成する
+      i_number_of_color_and_radius = np.zeros([6, m_dicSettingElements["MaxRadius"]], dtype=np.int)
+    np.savetxt(os.path.join(nstrResultFolderPath, "result/color_radius.csv"), i_number_of_color_and_radius, delimiter=",", fmt="%d")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### WaterShedを行う
 def doWatershedMethod(nimgRaw, nimgBindMask, nstrResultFolderPath, nbShowFlag, nbDetail, niCount):
   #デッドロック対策
   niCount += 1
@@ -141,8 +162,6 @@ def doWatershedMethod(nimgRaw, nimgBindMask, nstrResultFolderPath, nbShowFlag, n
     plt.show()
     plt.close()
   #calcCircularity(markers_watershed)
-
-  
   i_number_of_color_and_radius_high, npdata_number_of_color_and_radius_high, i_number_labels_high, center_high = DistincteLabels(nimgRaw, i_number_labels_high, markers_watershed, nstrResultFolderPath, nbShowFlag, nbDetail, center_high)
   nimgRaw[markers_watershed == -1] = [0,255,0]
   img_opening_closing[markers_watershed != 1] = 0
@@ -157,7 +176,6 @@ def doWatershedMethod(nimgRaw, nimgBindMask, nstrResultFolderPath, nbShowFlag, n
       center_high = np.append(center_high, center_low, axis=0)
   i_number_labels_high -= 1
   return True, i_number_labels_high, center_high, i_number_of_color_and_radius_high, npdata_number_of_color_and_radius_high
-
 ###  使用していない為コメント化  ###########################
 #def adjust(nimgSorce, nfAlpha=1.0, nfBeta=0.0):
 #    # 積和演算を行う。
@@ -165,7 +183,11 @@ def doWatershedMethod(nimgRaw, nimgBindMask, nstrResultFolderPath, nbShowFlag, n
 #    # [0, 255] でクリップし、uint8 型にする。
 #    return np.clip(img_dst, 0, 255).astype(np.uint8)
 ##########################################################
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### 円形度を計算する
 def calcCircularity(nlsLabelTable, niLabel, nbShowFlag):
   nlsLabelTableCopy = nlsLabelTable.copy()
   nlsLabelTableCopy[nlsLabelTable != niLabel] = 0
@@ -176,14 +198,17 @@ def calcCircularity(nlsLabelTable, niLabel, nbShowFlag):
     plt.clf()
   contour , hierarchy = cv2.findContours(nlsLabelTableCopy, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
   if len(contour) == 0:
-    #演習がとれないのなら0とする
+    #円周がとれないのなら0とする
     return 0
   perimeter = cv2.arcLength(contour[0], True)
   area = cv2.contourArea(contour[0])
   i_circle_level = (int)((4.0 * np.pi *area / (perimeter * perimeter) )* 100)
   return i_circle_level
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### イメージを表示させる
 def showPicture(nimgPicture, nstrTitle):
   #画像高さが1080pix以下になるようにリサイズする
   i_height, i_width = nimgPicture.shape[:2]
@@ -199,40 +224,47 @@ def showPicture(nimgPicture, nstrTitle):
     key = cv2.waitKey(1)
   #ウィンドウ情報を消去する
   cv2.destroyAllWindows()
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### 各ラベルを判別させる
 def DistincteLabels(nimgSrc, niLabelNumber, nlsLabelTable, nstrResultFolderPath, nbShowFlag, nbDetail, nCenterPoint):
-  m_dicSettingElements={"NumberPerOneMiliMeter" : "", "MaxRadius" : ""} #設定ファイルから読み込む変数群を格納した辞書型変数 
-  cHandleJsonfile.ReadElementsFromJsonfile(os.path.join(nstrResultFolderPath,"Setting.json"), 'mWaterShed', m_dicSettingElements)
   i_number_of_color_and_radius = np.zeros([6, m_dicSettingElements["MaxRadius"]], dtype=np.int) #(赤,黄,緑,青,紫,白)ごとの個数
   i_label_number = niLabelNumber
   npdata_number_of_color_and_radius = np.array([[0, 0]])
   for label in range(2, niLabelNumber + 1):
     i_circle_level = calcCircularity(nlsLabelTable, label, nbShowFlag)
-    if (i_circle_level >= 75):
+    if (i_circle_level >= m_dicSettingElements["circle_level_threshold"]):
       i_label_group_index = np.where(nlsLabelTable == label) #現ラベル数のブロブ情報
       i_array_label_bgr = nimgSrc[i_label_group_index] #rgb情報
       i_width = i_array_label_bgr.shape[0] #所属ブロブ個数
       #半径を決定
       i_ret_radius = CalculateRadius(i_width, m_dicSettingElements["MaxRadius"], m_dicSettingElements["NumberPerOneMiliMeter"])
+      #メディアンフィルター
+      img_label_bgr_median = cv2.medianBlur(i_array_label_bgr,5)
       img_label_bgr = np.zeros((1, i_width, 3), dtype='uint8') #rgb情報を格納するnumpy型変数
       #opencvのメソッドを使用するために一列の長い画像として情報を格納する
-      img_label_bgr[0, :, :] = i_array_label_bgr 
+      img_label_bgr[0, :, :] = img_label_bgr_median 
       #rgb→hsv変換
       img_hsv = cv2.cvtColor(img_label_bgr, cv2.COLOR_BGR2HSV)
       #hsvそれぞれの平均値を算出
-      h, s, v = cv2.split(img_hsv)
-      i_h_mean = h.mean()
-      i_s_mean = s.mean()
-      i_v_mean = v.mean()
+      hsv_list =  list(cv2.split(img_hsv))
+      #h, s, v = cv2.split(img_hsv)
+      #i_s_mean = hsv_list[1].mean()
+      #i_v_mean = hsv_list[2].mean()
+      hsv_without_white = [hsv_list[0][0][i] for i in range(hsv_list[0].shape[1]) if (hsv_list[1][0][i] > m_dicSettingElements["white_satuation"])]
 #      #デバックに使用する    
 #      b, g, r = cv2.split(img_label_bgr)
 #      i_b_mean = b.mean()
 #      i_g_mean = g.mean()
 #      i_r_mean = r.mean()
-      #明度が高く彩度が低いブロブを白色とし、その条件外のブロブはhueを使用して色を決定させる
-      if i_v_mean > 100 and i_s_mean < 100:
+      #明度が高く・彩度が低いブロブを白色とし、その条件外のブロブはhueを使用して色を決定させる
+      white_ratio = (hsv_list[0].shape[1]-len(hsv_without_white)) / hsv_list[0].shape[1]
+      if white_ratio > 0.9:
         i_ret_color = 5
       else:
+        i_h_mean = sum(hsv_without_white) / len(hsv_without_white)
         i_ret_color = DistincteColor(i_h_mean)
       i_number_of_color_and_radius[i_ret_color][i_ret_radius - 1] += 1
       npdata_color_and_radius = np.array([i_ret_color, i_ret_radius - 1])
@@ -244,12 +276,16 @@ def DistincteLabels(nimgSrc, niLabelNumber, nlsLabelTable, nstrResultFolderPath,
   npdata_number_of_color_and_radius = npdata_number_of_color_and_radius[1 : len(npdata_number_of_color_and_radius), : ]
   #(色, 半径)それぞれの個数を配列として返す
   return i_number_of_color_and_radius, npdata_number_of_color_and_radius, i_label_number, nCenterPoint
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### 色を判別させる
 def DistincteColor(niHue):
   #hueの値を(赤,黄,緑,青,紫)に区分けする為の区切り数値
-  i_color_band = (10,40,80,140,160)
+  i_color_band = (10,50,100,140,160)
   #色判定：赤
-  if i_color_band[0] > niHue <= i_color_band[4]:
+  if i_color_band[0] > niHue or niHue >= i_color_band[4]:
     i_ret = 0
   #色判定：黄
   elif i_color_band[0] <= niHue < i_color_band[1]:
@@ -265,7 +301,11 @@ def DistincteColor(niHue):
     i_ret = 4
   #hueを判定した色の要素のみに1を入れて返す
   return i_ret
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+### 半径を測定する
 def CalculateRadius(niNamberOfPixels, niMaxRadius, ndNumberPerOneMiliMeter):
   #半径毎の面積ピクセル数を計算し、比較する
   for i_radius in range(niMaxRadius):
@@ -275,7 +315,9 @@ def CalculateRadius(niNamberOfPixels, niMaxRadius, ndNumberPerOneMiliMeter):
       break
   #半径を返す
   return i_radius
-    
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
 
 if __name__ == '__main__':
-  main(True, "", True)
+  main(True, "", True, False)
